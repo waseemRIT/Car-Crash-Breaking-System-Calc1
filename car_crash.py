6d5f3411e1e01c0fbd1baa54c9db91fd131d76ad
waseemRIT <whq8052@g.rit.edu>
2021-10-30 15:32:20 +0400

    Drawing the wall!

--- car_crash.py
+++ car_crash.py
@@ -5,15 +5,30 @@ def car(IMAGE):
     screen = Screen()
     screen.register_shape(IMAGE)
 
-    turtle = Turtle()
-    turtle.shape(IMAGE)
-    turtle.forward(100)
-    x = input("")
+    car = Turtle()
+    car.shape(IMAGE)
+    screen.bgcolor("gray")
+    screen.title("Case 1 ")
+    car.penup()
+    car.forward(100)
+    car.pendown()
+    car.left(90)
+    car.forward(50)
+    car.right(180)
+    car.penup()
+    car.forward(50)
+    car.pendown()
+    car.forward(50)
+    exit1 = input("Press any key to exit!")
+
+
+
 
 
 def main():
     IMAGE = "car\car-2-icon-48.gif"
     car(IMAGE)
+
 
 
 if __name__ == '__main__':
