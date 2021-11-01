527a97a7642d507ceed4f7a9e393fd6f2e472816
waseemRIT <whq8052@g.rit.edu>
2021-11-01 09:17:31 +0400

    Ready to drive!!

--- car_crash.py
+++ car_crash.py
@@ -6,24 +6,25 @@ def go_to_start(car):
     car.goto(-210, 0)
     car.pendown()
 
+
 def case1_text(car):
     car.penup()
     car.goto(-280, 200)
     car.color("White")
-    car.write("Case 1: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline') )
+    car.write("Case 1: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline'))
     car.pendown()
+
 
 def case2_text(car):
     car.reset()
     car.penup()
     car.goto(-280, 200)
     car.color("White")
-    car.write("Case 2: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline') )
+    car.write("Case 2: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline'))
     car.pendown()
 
 
 def street(car):
-
     car.pencolor(0, 0, 0)
     car.left(90)
     car.forward(150)
@@ -37,10 +38,19 @@ def street(car):
     car.forward(100)
     car.right(90)
     car.penup()
-    car.forward(50)
+    car.forward(40)
     car.pendown()
 
-
+def wall(car):
+    car.color("white")
+    car.penup()
+    car.forward(350)
+    car.penup()
+    car.left(90)
+    car.forward(100)
+    car.pendown()
+    car.right(180)
+    car.forward(150)
 
 def car(IMAGE):
     screen = Screen()
@@ -48,12 +58,17 @@ def car(IMAGE):
     car = Turtle()
     car.shape(IMAGE)
     screen.colormode(255)
-    screen.bgcolor(112,	120,	100)
+    screen.bgcolor(112, 120, 100)
     screen.title("Case 1 ")
     case1_text(car)
     go_to_start(car)
     street(car)
-    
+    wall(car)
+    go_to_start(car)
+    car.penup()
+    car.left(90)
+    car.forward(40)
+    car.pendown()
 
 
     exit1 = input("Press any key to exit!")
