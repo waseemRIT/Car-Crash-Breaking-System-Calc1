bd69f08318fb57fb09b3ccda3c81ff2a7281e3c1
waseemRIT <whq8052@g.rit.edu>
2021-11-01 08:58:45 +0400

    Created Case 1 and Case 2 text Functions! Working on the Street Function!

--- car_crash.py
+++ car_crash.py
@@ -1,34 +1,53 @@
-from turtle import Screen, Turtle
+from turtle import *
 
+
+def go_to_start(car):
+    car.penup()
+    car.goto(-200, 0)
+    car.pendown()
+
+def case1_text(car):
+    car.penup()
+    car.goto(-280, 200)
+    car.color("White")
+    car.write("Case 1: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline') )
+    car.pendown()
+
+def case2_text(car):
+    car.reset()
+    car.penup()
+    car.goto(-280, 200)
+    car.color("White")
+    car.write("Case 1: ", font=("Arial", 32, 'normal', 'bold', 'italic', 'underline') )
+    car.pendown()
+
+
+def street(car):
+
+    car.pencolor(0, 0, 0)
+    car.left(90)
+    car.forward(150)
 
 def car(IMAGE):
     screen = Screen()
     screen.register_shape(IMAGE)
-
     car = Turtle()
     car.shape(IMAGE)
-    screen.bgcolor("gray")
+    screen.colormode(255)
+    screen.bgcolor(112,	120,	100)
     screen.title("Case 1 ")
-    car.penup()
-    car.forward(100)
-    car.pendown()
-    car.left(90)
-    car.forward(50)
-    car.right(180)
-    car.penup()
-    car.forward(50)
-    car.pendown()
-    car.forward(50)
+    case1_text(car)
+    go_to_start(car)
+    street(car)
+    
+
+
     exit1 = input("Press any key to exit!")
-
-
-
 
 
 def main():
     IMAGE = "car\car-2-icon-48.gif"
     car(IMAGE)
-
 
 
 if __name__ == '__main__':
